59b52114e54260cb41e5a764a8e0e9c48e4b08fe
CptNoHand <90973107+CptNoHand@users.noreply.github.com>
2022-08-06 03:59:56 +0200

    Update cl_config.lua

--- client/cl_config.lua
+++ client/cl_config.lua
@@ -5,43 +5,43 @@ Config.UseTarget = GetConvar('UseTarget', 'false') == 'true' -- Use qb-target in
 
 Config.BossMenus = {
     ['police'] = {
-        vector3(461.45, -986.2, 30.73),
+        vector3(448.4, -973.2, 30.6),
     },
     ['ambulance'] = {
-        vector3(335.46, -594.52, 43.28),
+        vector3(310.1, -599.43, 43.29),
     },
     ['realestate'] = {
-        vector3(-716.11, 261.21, 84.14),
+        vector3(-124.786, -641.486, 168.820),
     },
     ['taxi'] = {
-        vector3(907.24, -150.19, 74.17),
+        vector3(903.32, -170.55, 74.0),
     },
-    ['cardealer'] = {
-        vector3(-27.47, -1107.13, 27.27),
+    ['sunrise'] = {
+        vector3(-809.96, -208.66, 37.13),
     },
-    ['mechanic'] = {
-        vector3(-339.53, -156.44, 44.59),
+    ['redline'] = {
+        vector3(-605.41, -919.55, 23.89),
     },
 }
 
 Config.BossMenuZones = {
     ['police'] = {
-        { coords = vector3(461.45, -986.2, 30.73), length = 0.35, width = 0.45, heading = 351.0, minZ = 30.58, maxZ = 30.68 } ,
+        { coords = vector3(448.4, -973.2, 30.6), length = 0.35, width = 0.45, heading = 351.0, minZ = 30.58, maxZ = 30.68 } ,
     },
     ['ambulance'] = {
-        { coords = vector3(335.46, -594.52, 43.28), length = 1.2, width = 0.6, heading = 341.0, minZ = 43.13, maxZ = 43.73 },
+        { coords = vector3(310.1, -599.43, 43.29), length = 1.2, width = 0.6, heading = 341.0, minZ = 43.13, maxZ = 43.73 },
     },
     ['realestate'] = {
-        { coords = vector3(-716.11, 261.21, 84.14), length = 0.6, width = 1.0, heading = 25.0, minZ = 83.943, maxZ = 84.74 },
+        { coords = vector3(-124.786, -641.486, 168.820), length = 0.6, width = 1.0, heading = 25.0, minZ = 83.943, maxZ = 84.74 },
     },
     ['taxi'] = {
-        { coords = vector3(907.24, -150.19, 74.17), length = 1.0, width = 3.4, heading = 327.0, minZ = 73.17, maxZ = 74.57 },
+        { coords = vector3(903.32, -170.55, 74.0), length = 1.0, width = 3.4, heading = 327.0, minZ = 73.17, maxZ = 74.57 },
     },
-    ['cardealer'] = {
-        { coords = vector3(-27.47, -1107.13, 27.27), length = 2.4, width = 1.05, heading = 340.0, minZ = 27.07, maxZ = 27.67 },
+    ['sunrise'] = {
+        { coords = vector3(-809.96, -208.66, 37.13), length = 2.4, width = 1.05, heading = 340.0, minZ = 27.07, maxZ = 27.67 },
     },
-    ['mechanic'] = {
-        { coords = vector3(-339.53, -156.44, 44.59), length = 1.15, width = 2.6, heading = 353.0, minZ = 43.59, maxZ = 44.99 },
+    ['redline'] = {
+        { coords = vector3(-605.41, -919.55, 23.89),, length = 1.15, width = 2.6, heading = 353.0, minZ = 43.59, maxZ = 44.99 },
     },
 }
 
